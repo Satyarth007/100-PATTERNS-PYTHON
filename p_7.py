fc3113e53e8d058eabcbd8958e0786e2e611d108
SATYARTH <satyarthtiwari123@gmail.com>
2022-12-03 00:24:07 +0530

    p7

---
+++ p_7.py
@@ -0,0 +1,12 @@
+#square with 1,2.....n
+n=int(input("enter n :- "))
+for i in range(n):
+    for j in range(n):
+        print(str(j+1),end=" ")
+    print()
+#output:-
+ # enter n :- 4
+ # 1 2 3 4
+ # 1 2 3 4
+ # 1 2 3 4
+ # 1 2 3 4
