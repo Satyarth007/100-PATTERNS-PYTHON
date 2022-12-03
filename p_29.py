ae0935f0aef309a91c538e24c6d22d5357a5c742
SATYARTH <satyarthtiwari123@gmail.com>
2022-12-04 00:23:21 +0530

    p_29

---
+++ p_29.py
@@ -0,0 +1,8 @@
+# PYRAMID WITH * SYMBOL
+n=int(input("enter n:- "))
+for i in range(n):
+    print(" "*(n-i-1)+"* "*(i+1))
+
+            # IMPORTANT NOTE !
+            # no. of spaces in a row = n-i-1
+            # no. of stars in row = i+1
