a841cb246854df2db639c39e103f5e7c0a347ba6
SATYARTH <satyarthtiwari123@gmail.com>
2022-12-08 15:05:19 +0530

    p54

---
+++ p_54.py
@@ -0,0 +1,10 @@
+#right half diamond with alphabet symbols in dictionary order in every row
+n=int(input("enter n:- "))
+for i in range(n):
+       for j in range(i+1):
+           print(chr(65+j),end=" ")
+       print()
+for i in range(n-1):
+    for j in range(n-i-1):
+        print(chr(65+j),end=" ")
+    print()
