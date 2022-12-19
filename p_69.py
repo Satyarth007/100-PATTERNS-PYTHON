92b65a9753b66fd8ddbe586c3c9f5a1314061179
SATYARTH <satyarthtiwari123@gmail.com>
2022-12-19 17:23:29 +0530

    p_69

---
+++ p_69.py
@@ -0,0 +1,7 @@
+#bottom half hallow diamond pattern with * symbol
+n=int(input("enter n:- "))
+for i in range(n): # i =0,1,2,3,..........
+    print("  "*i + "* ",end="")
+    if i!=n-1:
+        print("  "*(2*n-2*i-3)+"* ")
+
